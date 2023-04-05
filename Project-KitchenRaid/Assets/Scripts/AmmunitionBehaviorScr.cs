ac73d07beffee3c99b0bafb080fda97153d60050
jethanstory <32407746+jethanstory@users.noreply.github.com>
2023-04-05 16:27:10 -0700

    added basic bullet replenish system

--- Project-KitchenRaid/Assets/Scripts/AmmunitionBehaviorScr.cs
+++ Project-KitchenRaid/Assets/Scripts/AmmunitionBehaviorScr.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AmmunitionBehaviorScr : MonoBehaviour
 {
     public int bulletCount = 7;
+    public int bulletNeeded = 0;
     public GameObject sliderBack;
     public GameObject sliderOriginal;
     // Start is called before the first frame update
@@ -26,6 +27,15 @@ public class AmmunitionBehaviorScr : MonoBehaviour
             sliderOriginal.SetActive(false);
             GameObject.Find("M1911 Handgun_Model_1").GetComponent<SimpleShoot>().enabled = false;
         }
+        bulletNeeded = 7 - bulletCount;
 
+    }
+
+    private void OnTriggerEnter(Collider other) // to see when the player enters the collider
+    {
+        if(other.gameObject.tag == "Ammo") //on the object you want to pick up set the tag to be anything, in this case "object"
+        {
+           bulletCount+=bulletNeeded;
+        }
     }
 }
