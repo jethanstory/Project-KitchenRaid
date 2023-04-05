193cb287ec236d3acd696fbcda1abb09cf09efe6
jethanstory <32407746+jethanstory@users.noreply.github.com>
2023-04-05 16:44:33 -0700

    Update AmmunitionBehaviorScr.cs

--- Project-KitchenRaid/Assets/Scripts/AmmunitionBehaviorScr.cs
+++ Project-KitchenRaid/Assets/Scripts/AmmunitionBehaviorScr.cs
@@ -36,6 +36,9 @@ public class AmmunitionBehaviorScr : MonoBehaviour
         if(other.gameObject.tag == "Ammo") //on the object you want to pick up set the tag to be anything, in this case "object"
         {
            bulletCount+=bulletNeeded;
+           sliderBack.SetActive(false);
+           sliderOriginal.SetActive(true);
+           GameObject.Find("M1911 Handgun_Model_1").GetComponent<SimpleShoot>().enabled = true;
         }
     }
 }
