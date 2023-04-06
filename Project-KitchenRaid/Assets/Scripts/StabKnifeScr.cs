0b5b851b6397945a3eb3c64fe05b7eb7acefeb03
jethanstory <32407746+jethanstory@users.noreply.github.com>
2023-04-05 17:01:06 -0700

    Moved some stuff and added temp knife stab

---
+++ Project-KitchenRaid/Assets/Scripts/StabKnifeScr.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StabKnifeScr : MonoBehaviour
+{
+    public GameObject knifeHand;
+
+    public float speed = 2.0f;
+
+    // The target (cylinder) position.
+    public Transform knifeTarget;
+    public Transform pocketTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            StabKnife();
+        }
+    }
+
+    public void StabKnife()
+    {
+        {
+            var step =  speed * Time.deltaTime; // calculate distance to move
+            knifeHand.transform.position = Vector3.MoveTowards(knifeHand.transform.position, knifeTarget.position, step);
+
+            // Check if the position of the cube and sphere are approximately equal.
+            if (Vector3.Distance(knifeHand.transform.position, knifeTarget.position) < 0.0001f)//< 0.001f)
+            {
+               knifeHand.transform.position = Vector3.MoveTowards(knifeHand.transform.position, pocketTarget.position, step);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) // to see when the player enters the collider
+    {
+        if(other.gameObject.tag == "Zombie") //on the object you want to pick up set the tag to be anything, in this case "object"
+        {
+           Debug.Log("HIT");
+            
+        }
+    }
+}
